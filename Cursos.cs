332625e9060dde5ce9fa39e5623adb621f5ec115
Julio <Julio@DESKTOP-DA92EIV>
2023-06-20 12:06:41 -0500

    Agregar archivos de proyecto.

---
+++ Cursos.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Cursos
+{
+    public class Curso
+    {
+        public int Grado { get; set; }
+
+        public Curso()
+        {
+        }
+    }
+
+    public class CursoPreEscolar : Curso
+    {
+
+        public CursoPreEscolar()
+        {
+
+        }
+
+    }
+
+    public class CursoBasica : Curso
+    {
+
+        public CursoBasica()
+        {
+
+        }
+
+    }
+
+    public class CursoBachillerato : Curso
+    {
+
+        public CursoBachillerato()
+        {
+
+        }
+
+    }
+
+    public class CursosPreEscolar 
+    { 
+
+        public CursoPreEscolar[] Curso { get; set; }
+
+        public CursosPreEscolar()
+        {
+
+            Curso = new CursoPreEscolar[2];
+
+            for (int i = 0; i < Curso.Length; i++)
+            {
+                Curso[i] = new CursoPreEscolar();
+            }
+
+        }
+
+        public void ListarCursos()
+        {
+            for(int i=0; i<Curso.Length; i++)
+            {
+                switch(i)
+                {
+                    case 0:
+                        Console.WriteLine("Primero de preescolar");
+                        break;
+                    case 1:
+                        Console.WriteLine("Segundo de preescolar");
+                        break;
+                }
+            }
+        }
+
+    }
+
+    public class CursosBasica
+    {
+
+        public CursoBasica[] Curso { get; set; }
+
+        public CursosBasica()
+        {
+
+            Curso[10] = new CursoBasica();
+        }
+
+    }
+
+    public class CursosBachillerato
+    {
+
+        public CursoBachillerato[] Curso { get; set; }
+
+        public CursosBachillerato()
+        {
+
+            Curso[10] = new CursoBachillerato();
+        }
+
+    }
+
+
+}
---
+++ UnidadEducativa.cs
@@ -0,0 +1,53 @@
+﻿using Personas;
+using System.Security.Cryptography;
+using Cursos;
+
+namespace UnidadEducativa
+{
+    public class UnidadEdu
+    {
+
+        public Profesor[] Profesores { get; set; }
+
+        public CursosPreEscolar cursosPreEscolar { get; set; }
+
+        public CursosBasica cursosBasica { get; set; }
+
+        public CursosBachillerato cursosBachillerato { get; set; }
+
+        public UnidadEdu()
+        {
+
+            cursosPreEscolar = new CursosPreEscolar();
+
+            Console.WriteLine("En el constructor de UnidadEducativa");
+            Console.WriteLine("Cuantos profesores va a tener:");
+            int CantProfesores = int.Parse(Console.ReadLine());
+
+            Profesores = new Profesor[CantProfesores];
+
+
+            for (int i = 0; i < CantProfesores; i++)
+            {
+                Profesores[i] = new Profesor();
+            }
+
+        }
+
+        public void ListarProfesores()
+        {
+            /*for(int i = 0;i < Profesores.Length;i++)
+            {
+                Console.WriteLine(i + " " + Profesores[i].Nombre);
+            }*/
+
+            int i = 0;
+            foreach(var profesor in Profesores)
+            {
+                Console.WriteLine((++i)+" "+profesor.Nombre+" "+ profesor.ObtEstadoAntiguedad());
+
+            }
+        }
+
+    }
+}
